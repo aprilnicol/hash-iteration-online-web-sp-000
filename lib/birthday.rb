d10ef2c301b2f6846c05475434a76dd8a0c24ee2
April Adams <illusionlace@gmail.com>
2020-09-02 03:40:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,14 +9,13 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
-end 
+end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age > 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else 
+    else
        puts "You are too old for this."
-     end 
-   end 
+     end
+   end
    
-      
